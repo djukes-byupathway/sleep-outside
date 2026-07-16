9dfd4cc44ceb96ee0bbadf8bb17c22b22b17937c
djukes-byupathway <djukes@byupathway.edu>
2026-07-16 15:05:33 -0500

    week 2 individual

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -31,3 +31,12 @@ export function getParam(param) {
 
   return urlParam;
 }
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
