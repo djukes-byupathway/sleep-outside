c2831465b57ac3a849c46f9aa3118179b981cdbe
djukes-byupathway <djukes@byupathway.edu>
2026-07-18 17:47:52 -0500

    fixed issue with productlist.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -36,25 +36,4 @@ export default class ProductList {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
 
     }
-
-        const list = await this.dataSource.getData();
-        this.renderList();
-    }
-
-    renderList() {
-        //         const htmlStrings = list.map(productCardTemplate);
-        //         this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
-        // }
-        renderListWithTemplate(productCardTemplate, this.listElement, list)
-    }
-}
-function productCardTemplate(product) {
-    return ` <li class="product-card">
-        <a href="product_pages/?product=">
-            <img src="" alt="Image of"/>
-            <h2 class="card__brand"></h2>
-            <h3 class="card__name"></h3>
-            <p class="product-card__price"></p>
-        </a>
-    </li>`
 }
