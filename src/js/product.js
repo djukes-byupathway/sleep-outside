c9e9478632a39cbc34213b451a3a36b339a05e6f
grandeurpawin <gpawin@byupathway.edu>
2026-07-20 19:23:37 +0800

    Fix product page and updat paths

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,13 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
-console.log(dataSource.findProductById(productId));
+const productDetails = new ProductDetails(productId, dataSource);
+productDetails.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
