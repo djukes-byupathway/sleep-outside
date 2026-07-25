0b7a292f4c90840f8dce11451d3bd00c424aa7e1
ashworks-l <valash1330@gmail.com>
2026-07-25 22:56:55 +0200

    ash

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,40 +1,67 @@
+import { getLocalStorage } from "./utils.mjs";
+
 export default class CheckoutProcess {
-    constructor(key, outputSelector) {
-        this.key = key;
-        this.outputSelector = outputSelector;
-        this.list = [];
-        this.itemTotal = 0;
-        this.shipping = 0;
-        this.tax = 0;
-        this.orderTotal = 0;
+  constructor(key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = [];
+    this.itemTotal = 0;
+    this.shipping = 0;
+    this.tax = 0;
+    this.orderTotal = 0;
+  }
+
+  init() {
+    this.list = getLocalStorage(this.key) || [];
+    this.calculateItemSubTotal();
+  }
+
+  calculateItemSubTotal() {
+    let totalItems = 0;
+
+    this.itemTotal = this.list.reduce((sum, item) => {
+      totalItems += item.quantity;
+      return sum + item.FinalPrice * item.quantity;
+    }, 0);
+
+    const subtotal = document.querySelector(
+      `${this.outputSelector} #subtotal`
+    );
+
+    subtotal.innerText = `$${this.itemTotal.toFixed(2)}`;
+
+    const items = document.querySelector(
+      `${this.outputSelector} #itemCount`
+    );
+
+    if (items) {
+      items.innerText = totalItems;
     }
-    
-    init() {
-        this.list = getLocalStorage(this.key);
-        this.calculateItemSummary();
-    }
+  }
 
-    calculateItemSubTotal() {
-        // calculate and display the total dollar amount of the items in the cart, and the number of items.
+  calculateOrderTotal() {
+    let totalItems = 0;
 
-    }
+    this.list.forEach((item) => {
+      totalItems += item.quantity;
+    });
 
-    calculateOrderTotal() {
-        // calculate the tax and shipping amounts. Add those to the cart total to figure out the order total
-        this.tax = (this.itemTotal ...)
-        this.shipping =
-            this.orderTotal =
+    this.tax = this.itemTotal * 0.06;
 
-            // display the totals.
-            this.displayOrderTotals();
-    }
-    
-    displayOrderTotals() {
-        // once the totals are all calculated display them in the order summary page
-        const tax = document.querySelector(`${this.outputSelector} #tax`);
+    this.shipping = totalItems > 0 ? 10 + (totalItems - 1) * 2 : 0;
 
+    this.orderTotal = this.itemTotal + this.tax + this.shipping;
 
-        tax.innerText = `$${this.tax.toFixed(2)}`;
-    }
+    this.displayOrderTotals();
+  }
 
+  displayOrderTotals() {
+    const tax = document.querySelector(`${this.outputSelector} #tax`);
+    const shipping = document.querySelector(`${this.outputSelector} #shipping`);
+    const total = document.querySelector(`${this.outputSelector} #orderTotal`);
+
+    tax.innerText = `$${this.tax.toFixed(2)}`;
+    shipping.innerText = `$${this.shipping.toFixed(2)}`;
+    total.innerText = `$${this.orderTotal.toFixed(2)}`;
+  }
 }
