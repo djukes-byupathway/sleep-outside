0ffbe0ca53f7c142aef68c687fbac2eb0b1b4946
grandeurpawin <gpawin@byupathway.edu>
2026-07-29 12:11:03 +0800

    making changes to product_listiing/ index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import Alert from "./Alert.js";
 
 loadHeaderFooter();
 
-// Load and display any announcement alerts
-const alert = new Alert();
-alert.render();
 
 
