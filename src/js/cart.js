5e264730a0cdd2d8ae1faf58ddb4fb596d79a47e
grandeurpawin <gpawin@byupathway.edu>
2026-08-03 08:52:09 +0800

    Added chnages to the color portion

--- src/js/cart.js
+++ src/js/cart.js
@@ -82,7 +82,7 @@ function cartItemTemplate(item) {
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
-  <p class="cart-card__color">${item.Colors[0]?.ColorName || "No color"}</p>
+  <p class="cart-card__color">${item.Colors?.[0]?.ColorName || "No color"}</p>
   <p class="cart-card__quantity">Qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
   <span class="cart-card__remove" data-id="${item.Id}">X</span>
