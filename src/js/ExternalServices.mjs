e09de054dfab918438823e93e9798a3d53cf0561
grandeurpawin <gpawin@byupathway.edu>
2026-08-03 09:14:05 +0800

    Added expiration for the backend to align with

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,10 +1,13 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-function convertToJson(res) {
+async function convertToJson(res) {
+    const data = await res.json();
+
     if (res.ok) {
-        return res.json();
+        return data;
     } else {
-        throw new Error("Bad Response");
+        console.log("API Error:", data);
+        throw new Error(JSON.stringify(data));
     }
 }
 
